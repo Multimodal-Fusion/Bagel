42eb64b27cf8d28248080c604167811df2157e2e
1jsingh <jaskirat.singh@anu.edu.au>
2025-07-21 17:59:38 -0700

    update

--- train/pretrain_unified_navit.py
+++ train/pretrain_unified_navit.py
@@ -359,7 +359,7 @@ def main():
             resume=training_args.wandb_resume,
             mode="offline" if training_args.wandb_offline else "online"
         )
-        wandb.config.update(training_args)
+        wandb.config.update(training_args, allow_val_change=True)
         wandb.config.update(model_args)
         wandb.config.update(data_args)
     else:
